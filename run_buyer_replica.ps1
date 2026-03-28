fa7c839171397ff09eeded61005d18eb28c8e7d8
Umar <tumarahmed@gmail.com>
2026-03-27 20:23:11 -0600

    chore: rename replicas to buyer_replicas

--- run_buyer_replica.ps1
+++ run_buyer_replica.ps1
@@ -1,7 +1,7 @@
 param([int]$n)
 # Always resolve relative to the script's location
 Set-Location $PSScriptRoot
-$envFile = "env.replica.$n"
+$envFile = "env.buyer.replica.$n"
 Get-Content $envFile | ForEach-Object {
     if ($_ -match "^([^#][^=]+)=(.*)$") {
         [System.Environment]::SetEnvironmentVariable($matches[1].Trim(), $matches[2].Trim(), "Process")
@@ -11,4 +11,4 @@ Set-Location db_layer\buyer
 python buyer.py
 
 # Usage:
-# .\run_replica.ps1 <replica_number>
+# .\run_buyer_replica.ps1 <replica_number>
--- startup.ps1
+++ startup.ps1
@@ -1,8 +1,8 @@
-.\run_replica.ps1 0
-.\run_replica.ps1 1
-.\run_replica.ps1 2
-.\run_replica.ps1 3
-.\run_replica.ps1 4
+.\run_buyer_replica.ps1 0
+.\run_buyer_replica.ps1 1
+.\run_buyer_replica.ps1 2
+.\run_buyer_replica.ps1 3
+.\run_buyer_replica.ps1 4
 
 .\run_seller_replica.ps1 0
 .\run_seller_replica.ps1 1
